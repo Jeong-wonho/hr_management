6413bfa74ba457b41c9995a2f765909b98b5ac12
Jeong-wonho <kallzzang@naver.com>
2024-01-03 14:43:58 +0900

    app modules change

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { TypeOrmConfigService } from './config/typeorm.config.service';
 import { ConfigModule } from '@nestjs/config';
 import { DepartmentModule } from './department/department.module';
 import { CustomApiModule } from './custom-api/custom-api.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -16,7 +18,7 @@ import { CustomApiModule } from './custom-api/custom-api.module';
     DepartmentModule,
     CustomApiModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return '안녕하세요. 에코앤리치에 지원하게 된 정원호라고 합니다. Api 명세는 <a href="https://github.com/Jeong-wonho/hr_management"> 이곳에서 확인할 수 있습니다.</a>';
   }
 }
